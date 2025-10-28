13896d67281e08b755d881b6e5aab863809c19df
Keruvx <jhonys_s2@hotmail.com>
2025-10-28 19:09:02 -0300

    Adiciona arquivos da Aula 1 e Atividade 2

---
+++ Aula-1.py
@@ -0,0 +1,5 @@
+#Atividade - 1
+tempc = float(input("Informe temperatura: "))
+tempf = (tempc*9/5) + 32
+print("Temperatura em Fahrenheit: ",tempf)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Informe nota: "))
+nota2 = float(input("Informe nota: "))
+nota3 = float(input("Informe nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+print("\n")
+if media <=6:
+    print("A baixo da media")
+else:
+    print("Acima da media")
