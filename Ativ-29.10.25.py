4a1a5f601df68075f38e86847ef2e5791ace074f
Keruvx <jhonys_s2@hotmail.com>
2025-10-29 19:30:06 -0300

    Aula de hoje

---
+++ Ativ-29.10.25.py
@@ -0,0 +1,17 @@
+##Simulador de emprestimo Bancario
+print("===Posso comprar?===")
+
+nome = input("Informe seu nome: ")##Primeiro vem o nome da variavel (Nome) depois (Input) para perguntar ao usuario
+renda = float(input("Informe sua renda: R$"))##float foi usado pq ser um numero Real
+valorSolicitado = float(input("Informe valor de emprestimo que deseja: R$"))
+parcela = int(input("Informe o número de parcelas: "))
+historico = input("Possui nome sujo? (Sim/Não): ")
+
+valorParcela = 
+
+
+
+
+
+
+
--- Aula-1-Atv2.py
+++ Aula-1-Atv2.py
@@ -1,2 +1,10 @@
 #Atividade - 2
-print("Teste")
+#Solicita ao usuário que informe a hora
+hora = int(input("Digite a hora atual (0 a 23): "))
+
+if hora <= 12:
+    print("Bom dia!")
+elif hora > 12 and hora < 18:
+    print("Boa tarde")
+else:
+    print("Boa noite")
